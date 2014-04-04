c1199236265c82d5e28ecd48065458dfd54a919e
Attila Večerek <xvecer17@stud.fit.vutbr.cz>
2014-04-04 09:57:37 +0200

    Class Game and example
    Skeleton of Class Game and an example of a labyrinth is pushed.

---
+++ FIT-IJA-ukol3/src/ija/homework3/game/Game.java
@@ -0,0 +1,31 @@
+package ija.homework3.game;
+
+import ija.homework3.tape.*;
+
+public class Game {
+
+	public Game()
+	{
+		
+	}
+	
+	public void startGame()
+	{
+		
+	}
+	
+	public void show()
+	{
+		
+	}
+	
+	public void print()
+	{
+		
+	}
+	
+	public void closeGame()
+	{
+		
+	}
+}
